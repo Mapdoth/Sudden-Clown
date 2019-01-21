c4958d1cc2c40820b6654614df3b44a6dddc66fb
aleixgab <aleix.gab@gmail.com>
2019-01-21 23:54:13 +0100

    nurse follow with navMesh

--- CITMGameJam/Assets/Scripts/Nurse_move.cs
+++ CITMGameJam/Assets/Scripts/Nurse_move.cs
@@ -1,32 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Nurse_move : MonoBehaviour {
 
-    static public bool player_seen;
-    public GameObject player;
-    public GameObject cone;
-    public float speed;
+    static public bool player_seen = false;
+    public Transform player;
+    public Transform cone;
+    public Transform nurse;
 
-    private float rotation;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
 	// Update is called once per frame
 	void FixedUpdate () {
 		
         if (player_seen)
         {
-            Vector2 dir = new Vector2(player.transform.position.x, player.transform.position.z) - new Vector2 (transform.position.x, transform.position.z);
-          
-            transform.transform.Translate(dir.normalized * speed);
-            cone.transform.LookAt(player.transform);
-            
-        }
+            GetComponent<NavMeshAgent>().destination = player.transform.position;
 
+            cone.transform.LookAt(player.transform);
+            nurse.transform.rotation = new Quaternion(0.7071f,0.0f,0.0f,0.7071f);
+        }
     }
 }
