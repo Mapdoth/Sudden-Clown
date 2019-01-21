dba76613dfa7b17bb3660894b8550d533f36f7a7
Acaree <acampamar@gmail.com>
2019-01-21 11:39:18 +0100

    Test character & player script

---
+++ CITMGameJam/Assets/Character/Scripts/Player_move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour {
+
+    // Use this for initialization
+
+
+    public float speed;
+ 
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0,speed,0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, -speed, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-speed,0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+
+
+    }
+}
