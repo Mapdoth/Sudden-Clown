1353cd7eb70e1a7b5e459658f2b27a8af4288bee
Acaree <acampamar@gmail.com>
2019-01-22 00:39:27 +0100

    old man & scripts from character

---
+++ CITMGameJam/Assets/Scripts/Surprise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surprise : MonoBehaviour {
+
+    static public int key_react = -1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+       if(key_react == 0 && Input.GetKeyDown("x"))
+        {
+            Debug.Log("Combo Breaker");
+        }
+
+	}
+}
