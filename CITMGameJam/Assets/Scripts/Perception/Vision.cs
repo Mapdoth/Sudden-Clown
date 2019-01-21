a97132c4137d28be1268e1afdeea61feb23cd8a9
Aleix Gabarró <aleix.gab@gmail.com>
2019-01-21 17:22:30 +0100

    class perception event

--- CITMGameJam/Assets/Scripts/Perception/Vision.cs
+++ CITMGameJam/Assets/Scripts/Perception/Vision.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PerceptionEvent
+{
+    public enum senses { VISION, SOUND };
+    public enum types { NEW, LOST };
+
+    public GameObject go;
+    public senses sense;
+    public types type;
+}
+
 public class Vision : MonoBehaviour {
 
     public Camera frustum;
