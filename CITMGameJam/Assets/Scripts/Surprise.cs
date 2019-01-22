e1dc717677839f7b2683b726d63d8d6ddc931c65
Acaree <acampamar@gmail.com>
2019-01-22 06:06:40 +0100

    Attack scripts working

--- CITMGameJam/Assets/Scripts/Surprise.cs
+++ CITMGameJam/Assets/Scripts/Surprise.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class Surprise : MonoBehaviour {
 
     static public int key_react = -1;
+    static public bool atacking = false;
+
+    private Animator animation;
+   
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+        animation = GetComponentInChildren<Animator>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
        if(key_react == 0 && Input.GetKeyDown("x"))
         {
-            Debug.Log("Combo Breaker");
+            atacking = true;
+            animation.SetBool("Attacking", atacking);
         }
 
-	}
+        if (animation.GetCurrentAnimatorStateInfo(0).IsName("clown_attack"))
+        {
+            atacking = false;
+            animation.SetBool("Attacking", atacking);
+        }
+
+    }
 }
