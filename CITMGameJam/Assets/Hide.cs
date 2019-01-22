64f9ad714b58be52e9cd6ce756ce20cecc4e0a88
JoanValiente <joan.valiente.lorite@gmail.com>
2019-01-22 11:30:31 +0100

    tusmuertosdavid

--- CITMGameJam/Assets/Hide.cs
+++ CITMGameJam/Assets/Hide.cs
@@ -1,82 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using XInputDotNetPure;
 using UnityEngine;
-
 
 public class Hide : MonoBehaviour {
 
-    public GameObject plant;
-    private bool hidden = false;
-    private GameObject player;
-
-    //button
-    bool playerIndexSet = false;
-    PlayerIndex playerIndex;
-    GamePadState state;
-    GamePadState prevState;
-    static public int key_react = -1;
-    private Vector3 dir;
-
-    private void Start()
-    {
-        player = GameObject.Find("Main_Character 1");
-    }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.name == "Main_Character 1")
         {
-            col.gameObject.SetActive(false);
-            plant.SetActive(true);
-            hidden = true;
+            Destroy(col.gameObject);
             //Haz cosas
         }
     }
 
-    private void Exit()
+    private void OnTriggerExit(Collider col)
     {
-        hidden = false;
-        player.SetActive(true);
-        plant.SetActive(false);
-        //Deja de hacer cosas
-    }
-
-    public void Update()
-    {
-
-        if (hidden == true)
+        if (col.gameObject.name == "Main_Character 1")
         {
-
-            if (!playerIndexSet || !prevState.IsConnected)
-            {
-                for (int i = 0; i < 4; ++i)
-                {
-                    PlayerIndex testPlayerIndex = (PlayerIndex)i;
-                    GamePadState testState = GamePad.GetState(testPlayerIndex);
-                    if (testState.IsConnected)
-                    {
-                        Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
-                        playerIndex = testPlayerIndex;
-                        playerIndexSet = true;
-                    }
-                }
-            }
-
-            prevState = state;
-            state = GamePad.GetState(playerIndex);
-
-            float x_motion = prevState.ThumbSticks.Left.X;
-            float y_motion = prevState.ThumbSticks.Left.Y;
-
-            dir.x = x_motion;
-            dir.z = y_motion;
-
-            if (state.Triggers.Left != 0 && dir != Vector3.zero)
-            {
-                player.transform.Translate(dir.normalized * 0.5f);
-                Exit();
-            }
+            Destroy(col.gameObject);
+            //Deja de hacer cosas
         }
     }
 }
