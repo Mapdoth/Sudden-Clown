cdfdf0f6cd8b0c1a0ea10f6315954b0218b02952
DavidLozanoSánchez <davidthemaaster@gmail.com>
2019-01-22 06:08:38 +0100

    Added Nurse Sprite and Animations

--- CITMGameJam/Assets/Character/Scripts/Player_move.cs
+++ CITMGameJam/Assets/Character/Scripts/Player_move.cs
@@ -9,10 +9,14 @@ public class Player_move : MonoBehaviour {
 
     public float speed;
     private Vector3 dir;
+    private Animator animation;
+    private SpriteRenderer flip;
 
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        animation = GetComponentInChildren<Animator>();
+        flip = GetComponentInChildren<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update() {
@@ -21,28 +25,36 @@ public class Player_move : MonoBehaviour {
 
         if (Input.GetKey("w"))
         {
+            animation.SetBool("Walk", true);
             dir.z = 1.0f;
         }
 
         if (Input.GetKey("s"))
         {
+            animation.SetBool("Walk", true);
             dir.z = -1.0f;
         }
 
         if (Input.GetKey("a"))
         {
             dir.x = -1.0f;
+            flip.flipX = true;
+            animation.SetBool("Walk", true);
         }
 
         if (Input.GetKey("d"))
         {
+            flip.flipX = false;
             dir.x = 1.0f;
+            animation.SetBool("Walk", true);
         }
-
-        if(dir != Vector3.zero)
+        if (dir != Vector3.zero)
         {
             transform.Translate(dir.normalized * speed);
         }
-
+        else
+        {
+            animation.SetBool("Walk", false);
+        }
     }
 }
--- CITMGameJam/Assets/Scripts/Nurse_move.cs
+++ CITMGameJam/Assets/Scripts/Nurse_move.cs
@@ -6,12 +6,25 @@ using UnityEngine.AI;
 public enum NurseMovements { None, Patrol, FollowPlayer, GoToSound};
 public class Nurse_move : MonoBehaviour {
 
+    public float range = 3.0f;
     public Transform player;
     public Transform cone;
     public Transform nurse;
     static public GameObject sound_emiter;
     static public NurseMovements movements;
 
+    private Animator animator;
+    private Vector3 aux_position;
+    private SpriteRenderer flip;
+    private bool attacking;
+
+
+    private void Start()
+    {
+        aux_position = transform.position;
+        animator = GetComponentInChildren<Animator>();
+        flip = GetComponentInChildren<SpriteRenderer>();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -21,21 +34,58 @@ public class Nurse_move : MonoBehaviour {
                 break;
             case NurseMovements.Patrol:
                 cone.transform.localRotation = new Quaternion(0.0f,0.0f,0.0f,0.0f);
-
                 break;
-            case NurseMovements.FollowPlayer:
+            case NurseMovements.FollowPlayer:   
                 GetComponent<NavMeshAgent>().destination = player.transform.position;
                 cone.transform.LookAt(player.transform);
                 break;
 
             case NurseMovements.GoToSound:
-
                 GetComponent<NavMeshAgent>().destination = sound_emiter.transform.position;
-                
-
                 break;
         }
 
         nurse.transform.rotation = new Quaternion(0.7071f, 0.0f, 0.0f, 0.7071f);
+
+
+        //ANIMATIONS
+        if(transform.position.x > aux_position.x)
+        {
+            animator.SetBool("Walk", true);
+            flip.flipX = false;
+            aux_position = transform.position;
+        }
+        else if (transform.position.x == aux_position.x && transform.position.z == aux_position.z)
+        {
+            animator.SetBool("Walk", false);
+        }
+        else
+        {
+            animator.SetBool("Walk", true);
+            flip.flipX = true;
+            aux_position = transform.position;
+        }
+
+        //PLAYER ATTACK
+        float distance = (transform.position - player.transform.position).magnitude;
+        if (distance <= range)
+        {
+            attacking = true;
+        }
+        else
+        {
+            attacking = false;
+        }
+
+        if(attacking)
+        {
+            animator.SetBool("isAttacking", true);
+            GetComponent<NavMeshAgent>().destination = transform.position;
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+
     }
 }
