0f9a320b53f15ea842f09cd5be274331dbaa33c9
Acaree <acampamar@gmail.com>
2019-01-22 09:16:03 +0100

    XBOX controller added

--- CITMGameJam/Assets/Character/Scripts/Player_move.cs
+++ CITMGameJam/Assets/Character/Scripts/Player_move.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using XInputDotNetPure;
 using UnityEngine;
 
 public class Player_move : MonoBehaviour {
 
-    // Use this for initialization
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    //button things
 
 
     public float speed;
@@ -18,36 +24,61 @@ public class Player_move : MonoBehaviour {
         flip = GetComponentInChildren<SpriteRenderer>();
     }
 
+   void FixedUpdate()
+    {
+        GamePad.SetVibration(playerIndex, state.Triggers.Left, state.Triggers.Right);
+    }
+
     // Update is called once per frame
     void Update() {
 
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+        //button things
+
         dir = Vector3.zero;
 
-        if (Input.GetKey("w"))
+        float x_motion = state.ThumbSticks.Left.X;
+        float y_motion = state.ThumbSticks.Left.Y;
+
+        if (y_motion != 0)
         {
             animation.SetBool("Walk", true);
-            dir.z = 1.0f;
+       
         }
 
-        if (Input.GetKey("s"))
+        if (x_motion < 0)
         {
-            animation.SetBool("Walk", true);
-            dir.z = -1.0f;
-        }
-
-        if (Input.GetKey("a"))
-        {
-            dir.x = -1.0f;
             flip.flipX = true;
             animation.SetBool("Walk", true);
         }
 
-        if (Input.GetKey("d"))
+        if (x_motion > 0)
         {
             flip.flipX = false;
-            dir.x = 1.0f;
             animation.SetBool("Walk", true);
         }
+
+        dir.x = x_motion;
+        dir.z = y_motion;
+
         if (dir != Vector3.zero)
         {
             transform.Translate(dir.normalized * speed);
--- CITMGameJam/Assets/Scripts/Surprise.cs
+++ CITMGameJam/Assets/Scripts/Surprise.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using XInputDotNetPure;
 using UnityEngine;
 
 public class Surprise : MonoBehaviour {
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    //button things
+
 
     static public int key_react = -1;
     static public bool atacking = false;
@@ -19,8 +28,47 @@ public class Surprise : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-       if(key_react == 0 && Input.GetKeyDown("x"))
+
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+
+        //button things
+
+        if (key_react == 0 && prevState.Buttons.X == ButtonState.Pressed)
+        {
+            atacking = true;
+            animation.SetBool("Attacking", atacking);
+        }
+
+        if (key_react == 1 && prevState.Buttons.Y == ButtonState.Pressed)
+        {
+            atacking = true;
+            animation.SetBool("Attacking", atacking);
+        }
+
+        if (key_react == 2 && prevState.Buttons.A == ButtonState.Pressed)
+        {
+            atacking = true;
+            animation.SetBool("Attacking", atacking);
+        }
+
+        if (key_react == 0 && prevState.Buttons.B == ButtonState.Pressed)
         {
             atacking = true;
             animation.SetBool("Attacking", atacking);
