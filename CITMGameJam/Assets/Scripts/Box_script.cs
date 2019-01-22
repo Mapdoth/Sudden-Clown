50be72ca129c8140943bcf13641e59d5cf4b581e
Acaree <acampamar@gmail.com>
2019-01-22 12:46:50 +0100

    R2 now generate a box :D

---
+++ CITMGameJam/Assets/Scripts/Box_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_script : MonoBehaviour {
+
+    // Use this for initialization
+
+    private float current_time;
+    public float time_to_explode;
+    public float soundtime;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        current_time += Time.deltaTime;
+
+        if(current_time >= time_to_explode)
+        {
+            GetComponent<SphereCollider>().enabled = true;
+
+            if(current_time >= time_to_explode + soundtime)
+            {
+                GetComponent<SphereCollider>().enabled = false;
+            }
+        }
+
+	}
+}
