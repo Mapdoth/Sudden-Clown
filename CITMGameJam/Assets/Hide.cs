85e89b7850c9be65b0f61692dbb5570fa4c9af22
JoanValiente <joan.valiente.lorite@gmail.com>
2019-01-22 09:21:58 +0100

    sergio's GILF

---
+++ CITMGameJam/Assets/Hide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Main_Character 1")
+        {
+            Destroy(col.gameObject);
+            //Haz cosas
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "Main_Character 1")
+        {
+            Destroy(col.gameObject);
+            //Deja de hacer cosas
+        }
+    }
+}
